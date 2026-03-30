8f10c83326d3c8a0c404bf8be5532e5c09e3b1df
mariamshalhoub93-art <mariamshalhoub93@gmail.com>
2026-03-30 15:58:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -19,5 +20,5 @@ def products():
     return render_template("products.html")
 
 if __name__ == '__main__':
-    port= int(os.envin.get("PORT" , 5000))
-    app.run(host='0.0.0.0' , port=port ,debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
